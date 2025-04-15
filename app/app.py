d01bf264855517144b9dc661fa462811777d5dab
branDevSecOps <brandevops@gmail.com>
2025-04-15 17:53:34 -0400

    change to app

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "🚀 Hello from @Brandevops on YouTube !"
+    return "🚀 @Brandevops on YouTube ! Go Subscribe to my channel"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
