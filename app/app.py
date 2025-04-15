91926ff6f3142ba5d28106a514622f1e99be9f08
branDevSecOps <brandevops@gmail.com>
2025-04-15 17:59:55 -0400

    retrying deployment

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "🚀 @Brandevops on YouTube ! Go Subscribe to my channel"
+    return "🚀 @Brandevops on YouTube ! Go Subscribe To My Channel"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
